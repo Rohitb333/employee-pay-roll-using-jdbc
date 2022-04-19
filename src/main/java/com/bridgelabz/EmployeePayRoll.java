bda89ed43010e155d9fb523d8a931c9a2dd5bf91
Rohit <rohitb333@gmail.com>
2022-04-19 12:38:35 +0530

    Added Establish Connection File

--- src/main/java/com/bridgelabz/EmployeePayRoll.java
+++ src/main/java/com/bridgelabz/EmployeePayRoll.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class EmployeePayRoll {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee jdbc program");
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
     }
 }
